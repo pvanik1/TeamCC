bcd8d097e4aad8ceedc41976dd22d90bf4870c46
Matej Poliacek <poliacek.matej@gmail.com>
2017-02-11 22:09:46 +0000

    static and media url stuff

--- stirhackproject/stirhackproject/settings.py
+++ stirhackproject/stirhackproject/settings.py
@@ -16,9 +16,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-
+STATIC_DIR = os.path.join(BASE_DIR, '/static/')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = MEDIA_DIR
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,11 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 #<<<<<<< HEAD
-STATIC_URL = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
-#=======
 
+MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]
+MEDIA_ROOT = MEDIA_DIR
 
 
 # PWA config
--- stirhackproject/stirhackproject/urls.py
+++ stirhackproject/stirhackproject/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
 from fixit import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
     url('', include('pwa.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
