00d71a676bf4a935321725af0bc370eafc2fb142
Matej Poliacek <poliacek.matej@gmail.com>
2017-02-12 03:07:35 +0000

    populate fixit, admin

--- stirhackproject/fixit/admin.py
+++ stirhackproject/fixit/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from fixit.models import Issue
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ('title', 'location_bdg', 'location_detail', 'upvotes', 'images')
 
 # Register your models here.
+
+admin.site.register(Issue, IssueAdmin)
