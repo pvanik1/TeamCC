be61f67e44c1853a4f4350284a3d7ff5c6ccb416
David Skillman <david.skillman@hotmail.com>
2017-02-12 01:01:08 +0000

    updated settings.py ALLOWED_HOSTS

--- stirhackproject/stirhackproject/settings.py
+++ stirhackproject/stirhackproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '^xzonalpt3(3nhh-6hokx@=3ffpobm-d%^2vt+_9r@cir&4r_^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
