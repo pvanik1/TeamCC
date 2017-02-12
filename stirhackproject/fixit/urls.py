77b553b03f4c3a62380a290c0bdec8ae6eac0272
retepvan <retepvan@yahoo.co.uk>
2017-02-12 10:43:15 +0000

    added camera.html

--- stirhackproject/fixit/urls.py
+++ stirhackproject/fixit/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 	url(r'^index/$', views.index, name='index'),
 	url(r'^logout/$', views.logout, name='logout'),
 	url(r'^addIssue/$', views.addIssue, name='addIssue'),
+	url(r'^register/$', views.register, name='register'),
 ]
