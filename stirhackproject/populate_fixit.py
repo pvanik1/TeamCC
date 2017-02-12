c8a073ea3fde1c1fb340c526f88bca951b1978f7
Matej Poliacek <poliacek.matej@gmail.com>
2017-02-12 03:10:14 +0000

    add files - populate_fixit

---
+++ stirhackproject/populate_fixit.py
@@ -0,0 +1,52 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+              'stirhackproject.settings')
+
+import django
+import random
+django.setup()
+from fixit.models import Issue
+
+
+def populate():
+
+    issues = [
+        {"title": "Uni avenue pothole",
+         "location_bdg": "N/A",
+         "location_detail": "Outside main gate",
+         "upvotes": "3",
+         "images": "Pothole.jpg",
+            },
+
+        {"title": "Slow PC",
+         "location_bdg": "Boyd Orr",
+         "location_detail": "Lvl 10, machine 5",
+         "upvotes": "24",
+         "images": "Pothole.jpg",
+            },
+
+        {"title": "Broken door handle",
+         "location_bdg": "Library",
+         "location_detail": "Level 8 annex",
+         "upvotes": "1",
+         "images": "Pothole.jpg",
+            },
+
+        {"title": "Heating not working",
+         "location_bdg": "Adam Smith 1115",
+         "location_detail": "top row",
+         "upvotes": "9",
+         "images": "Pothole.jpg",
+            },]
+
+    for issue in issues:
+        add_issue(issue["title"], issue["location_bdg"], issue["location_detail"], issue["upvotes"], issue["images"] )
+
+def add_issue(title, location_bdg, location_detail, upvotes, images):
+    i = Issue.objects.get_or_create(title=title, location_bdg=location_bdg, location_detail=location_detail, upvotes=upvotes, images=images)[0]
+    i.save()
+    return i
+
+if __name__ == '__main__':
+    print("Starting FixIt population script...")
+    populate()
